4c4e6b5f711c19b41187a90ee6341543d9107192
GunPKate <gunPK>
2023-02-05 18:27:18 +0700

    save and load customer

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,3 +1,6 @@
+import { Customer } from './../interface/customer';
+import { ShareService } from './../sharedservice';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerComponent implements OnInit {
 
-  constructor() { }
+  code:   String| undefined = "x00"
+  name:   String| undefined = "xxx"
+  email:  String| undefined = "@mail.com"
+  tel:    String| undefined = "0832221111"
+  lineId: String| undefined  
+  address: String| undefined ="BKK"
+
+  customer: Customer[] = [] 
+
+  constructor(
+    private http:HttpClient,
+    private  shareService:ShareService
+  ) { }
 
   ngOnInit(): void {
+    this.laod()
   }
 
+  save(){
+    if(confirm("Save Customer?")){
+      let params = {
+        code : this.code,
+        name : this.name,
+        email : this.email,
+        tel : this.tel,
+        lineId : this.lineId,
+        address : this.address
+      }
+  
+      let path = this.shareService.serverPath+"/customerSave"
+      this.http.post(path,params).subscribe((res:any)=>{alert("save successful")})
+    }
+  }
+
+  laod(){
+    let path = this.shareService.serverPath+"/customerGet"
+    this.http.get(path).subscribe((res:any)=>{
+      console.log("customer",res);
+      this.customer = res
+    })
+  }
 }
---
+++ src/app/interface/customer.ts
@@ -0,0 +1,8 @@
+export interface Customer{
+    code:   String
+    name:   String
+    email:  String
+    tel:    String
+    lineId: String
+    address: String
+}
