7c635ff9493227756db53b4a19310abae226ddb1
GunPKate <gunPK>
2023-02-16 22:33:44 +0700

    add customers service and html

--- src/app/checkup/checkup.component.ts
+++ src/app/checkup/checkup.component.ts
@@ -1,4 +1,7 @@
+import { ShareService } from './../sharedservice';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../interface/customer';
 
 @Component({
   selector: 'app-checkup',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckupComponent implements OnInit {
 
-  constructor() { }
+  customers: any
+  customer : any = {
+    name: null,
+    code: null,
+    _id: null
+  }
+  _id: string | undefined | null = null
+
+  constructor(private shareService:ShareService, private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.loadCustomer()
+  }
+
+  loadCustomer(){
+    let path = this.shareService.serverPath+"/customerGet"
+    this.http.get(path).subscribe((res:any)=>{
+      console.log("customer",res);
+      this.customers = res
+    })
+    this._id = null;
+    console.log(this.customers); 
+  }
+
+  chooseCustomer(customer: Customer){
+    this.customer = customer;
   }
 
 }
