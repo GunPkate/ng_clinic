eed2ba6fd766bc95b171de0911f6dc2e9d04a5f9
GunPKate <gunPK>
2023-03-06 21:44:54 +0700

    report symptom

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,3 +1,5 @@
+import { ShareService } from './../sharedservice';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
+  // search = {
+  //   from: null,
+  //   to: Date.now().toLocaleString()
+  // }
+  search = { from: "2023-03-01", to: "2023-03-06" }
+  reports : any = {}
+  totalPrice : number = 0
+
+  constructor(private http:HttpClient, private shareService:ShareService) { }
 
   ngOnInit(): void {
+  }
+
+  showReport(){
+    let sum = 0;
+    this.http.post(this.shareService.serverPath+"/getReport",this.search).subscribe((res)=>{this.reports = res})
+    console.log(this.reports);
+    for(let i =0; i < this.reports.length; i++){
+      this.reports[i].price? sum += this.reports[i].price : sum += 0;
+      console.log(i,this.reports[i].price)
+    }
+    this.totalPrice = sum
   }
 
 }
